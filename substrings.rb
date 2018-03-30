fc5bcbdeea2e4039a160ff1a08c899b000db4c95
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2018-03-29 21:52:12 -0700

    Finished Substrings

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,12 @@
- 
+def substrings(word, dictionary)
+  output = Hash.new(0)
+
+  dictionary.each do |i|
+  	output[i] = word.scan(/#{i}/).count
+  end
+  
+  output
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
