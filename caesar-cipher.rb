f935cd80b80cb63e00bc1027de4d2ab06b4ff197
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2018-03-29 20:36:28 -0700

    Finished Caesar Cipher

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,1 +1,15 @@
- 
+def caesar_cipher(phrase, shift)
+  output = phrase.split("")
+  output.map! do |letter| 
+  	if letter.ord.between?(65, 90)
+  	  letter = letter.ord + shift <= 90 ? (letter.ord + shift).chr : (letter.ord + shift - 90).chr
+  	elsif letter.ord.between?(97, 122)
+  	  letter = letter.ord + shift <= 122 ? (letter.ord + shift).chr : (letter.ord + shift - 122).chr
+  	else
+  	  letter
+  	end
+  end
+  output.join("")
+end 
+
+puts caesar_cipher("What a string!", 5)
