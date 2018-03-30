407941fcc581a725426395fc81a035413d12f119
Jon Chanyoung Yoo <jonyoowa@outlook.com>
2018-03-29 21:30:10 -0700

    Finished Stock Picker

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,1 +1,16 @@
- 
+def stock_picker(prices)
+  if prices.length <= 1
+  	puts "Please have at least two dates"
+  else
+  	maxProfit = prices[1] - prices[0]
+    prices.each do |element| 
+      subarray = prices.drop(prices.index(element))
+      subarray.each do |n|
+      	maxProfit = n - subarray[0] if (n - subarray[0]) > maxProfit
+      end
+    end
+    puts "Made a profit of #{maxProfit}"
+  end
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
